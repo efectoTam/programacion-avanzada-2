fb999e1e941bac6b85cfad55510d3ce5aad12a3c
Tamara <tam.luque@gmail.com>
2021-10-16 12:51:16 -0300

    changing tabs purpose

--- ventana.py
+++ ventana.py
@@ -12,20 +12,20 @@ nb.pack(fill="both", expand="yes")
 tab1 = ttk.Frame(nb)
 tab2 = ttk.Frame(nb)
 tab3 = ttk.Frame(nb)
-nb.add(tab1, text = "Búsqueda por nombre")
-nb.add(tab2, text = "Donar libro")
-nb.add(tab3, text = "Libros donados")
+nb.add(tab1, text = "Todos los libros")
+nb.add(tab2, text = "Búsqueda por nombre")
+nb.add(tab3, text = "Donar libro")
 
 # Contenido pestaña 1
-titulo = Label(tab1, text="Escribe el nombre del libro")
+titulo = Label(tab1, text="Estos son todos los libros que tenemos en la biblioteca")
 titulo.place(x=10, y=10)
 
 # Contenido pestaña 2
-titulo = Label(tab2, text="Llena los datos del libro que quieres donar")
+titulo = Label(tab2, text="Escribe el nombre del libro")
 titulo.place(x=10, y=10)
 
 # Contenido pestaña 3
-titulo = Label(tab3, text="Este es el listado de libros donados recientemente")
+titulo = Label(tab3, text="Ingresa los datos del libro que quieres donar")
 titulo.place(x=10, y=10)
 
 ventanaBiblioteca.mainloop()
