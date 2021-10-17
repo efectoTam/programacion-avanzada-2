6e9f40e5e944a3bd052f328be5a65f2ba546c90b
Tamara <tam.luque@gmail.com>
2021-10-16 23:53:30 -0300

    insert without a array

--- ventana.py
+++ ventana.py
@@ -57,6 +57,26 @@ class Libro:
             except Exception as e:
                 raise
 
+    # Método cuando se hace clic en Donar
+    def donarLibro(self):
+        dato1 = inputDonado1.get()
+        dato2 = inputDonado2.get()
+        dato3 = inputDonado3.get()
+        dato4 = inputDonado4.get()
+
+        if dato1 == '' or dato2 == '' or dato3 == '' or dato4 == '':
+            mensaje = "Debes ingresar toda la información solicitada."
+            textoDonacion['text'] = mensaje
+        else:
+            sql = "INSERT INTO donaciones(nombreLibro, autor, añoPublicacion, nombreColaborador) values ('{}', '{}', '{}', '{}')".format(dato1, dato2, dato3, dato4)
+            try:
+                self.cursor.execute(sql)
+                self.connection.commit()
+                mensaje = "Gracias por tu donación. Por favor, deja el libro dentro del buzón correspondiente."
+                textoDonacion['text'] = mensaje
+            except Exception as e:
+                raise
+
 libro1 = Libro()
 libro1.selectLibros()
 
@@ -120,7 +140,41 @@ botonResultado = Button(tab2, text="Buscar", command=libro1.buscarPorNombre, wid
 botonResultado.pack(anchor="w")
 
 # Contenido pestaña 3
-titulo3 = Label(tab3, text="Ingresa los datos del libro que quieres donar")
+titulo3 = Label(tab3, text="Ingresa los datos solicitados")
 titulo3.place(x=10, y=10)
+
+labelNombreDonado = Label(tab3, text="Nombre del libro")
+labelNombreDonado.place(x=10, y=40)
+input1 = StringVar()
+inputDonado1 = Entry(tab3, textvariable=input1, width="60", highlightthickness=1)
+inputDonado1.config(highlightbackground = "#000000", highlightcolor = "#000000")
+inputDonado1.place(x=10, y=70)
+
+labelApellidoDonado = Label(tab3, text="Autor")
+labelApellidoDonado.place(x=10, y=100)
+input2 = StringVar()
+inputDonado2 = Entry(tab3, textvariable=input2, width="60", highlightthickness=1)
+inputDonado2.config(highlightbackground = "#000000", highlightcolor = "#000000")
+inputDonado2.place(x=10, y=130)
+
+labelPublicacionDonado = Label(tab3, text="Año de publicación")
+labelPublicacionDonado.place(x=10, y=160)
+input3 = StringVar()
+inputDonado3 = Entry(tab3, textvariable=input3, width="60", highlightthickness=1)
+inputDonado3.config(highlightbackground = "#000000", highlightcolor = "#000000")
+inputDonado3.place(x=10, y=190)
+
+labelColaboradorDonado = Label(tab3, text="Tu nombre")
+labelColaboradorDonado.place(x=10, y=220)
+input4 = StringVar()
+inputDonado4 = Entry(tab3, textvariable=input4, width="60", highlightthickness=1)
+inputDonado4.config(highlightbackground = "#000000", highlightcolor = "#000000")
+inputDonado4.place(x=10, y=250)
+
+botonDonacion = Button(tab3, text="Donar", command=libro1.donarLibro, width="40")
+botonDonacion.place(x=10, y=300)
+
+textoDonacion = Label(tab3, text='')
+textoDonacion.place(x=10, y=340)
 
 ventanaBiblioteca.mainloop()
